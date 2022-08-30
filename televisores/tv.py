93b9a41e671f708b3c620997592105108e1209a0
Lema25 <56377966+Lema25@users.noreply.github.com>
2022-08-29 23:58:09 -0500

    Add files via upload

--- televisores/tv.py
+++ televisores/tv.py
@@ -12,36 +12,36 @@ class TV:
         self.control = None
         TV.numTV += 1
 
-    def getMarca(self):
-        return self.marca
     def setMarca(self, marca):
         if isinstance(marca, Marca):
             self.marca = marca
+    def getMarca(self):
+        return self.marca
 
-    def getControl(self):
-        return self.control
     def setControl(self, control):
         if isinstance(control, Control):
             self.control = control
+    def getControl(self):
+        return self.control
 
-    def getPrecio(self):
-        return self.precio
     def setPrecio(self, precio):
         self.precio = precio
+    def getPrecio(self):
+        return self.precio
 
-    def getVolumen(self):
-        return self.volumen
     def setVolumen(self, volumen):
         if(self.estado==True):
             if(volumen>=0 and volumen<=7):
                 self.volumen = volumen
+    def getVolumen(self):
+        return self.volumen
 
-    def getCanal(self):
-        return self._canal
     def setCanal(self, canal):
         if(self.estado==True):
             if(canal>=1 and canal<=120):
                 self.canal = canal
+    def getCanal(self):
+        return self._canal
 
     def turnOn(self):
         self.estado = True
